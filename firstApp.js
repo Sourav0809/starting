141d98f4b34e2dedb390e1cc2eed55d09a090311
Sourav0809 <spathak7431@gmail.com>
2023-11-18 13:06:42 +0530

    Routing created

--- firstApp.js
+++ firstApp.js
@@ -2,7 +2,33 @@ const http = require('http')
 
 const server = http.createServer((req, res) => {
     console.log(res)
-    console.log('Hello Sourav')
+
+    if (req.url === "/home") {
+        res.write('<html>')
+        res.write('<body><h1> Hello this is a home page </h1></body>')
+        res.write('</html>')
+        return res.end()
+    }
+    else if (req.url === "/about") {
+        res.write('<html>')
+        res.write('<body><h1> Welcome to ABout Page  </h1></body>')
+        res.write('</html>')
+        return res.end()
+    }
+    else if (req.url === "/node") {
+        res.write('<html>')
+        res.write('<body><h1> Welcome to node js project  </h1></body>')
+        res.write('</html>')
+        return res.end()
+    }
+    else {
+        res.write('<html>')
+        res.write('<body><h1> Welcome to Home Page   </h1></body>')
+        res.write('</html>')
+        return res.end()
+    }
+
+
 })
 
-server.listen(4000) 
+server.listen(4000)
