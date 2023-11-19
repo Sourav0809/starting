98c3867f20ad0fdbad1578594e5d7b9da184c0b7
Sourav0809 <spathak7431@gmail.com>
2023-11-19 19:10:26 +0530

    FE : CLEAN UP CODE

--- firstApp.js
+++ firstApp.js
@@ -1,48 +1,9 @@
 const http = require('http')
-const fs = require('fs')
+const routes = require('./routes.js')
+
 
 const server = http.createServer((req, res) => {
-
-    console.log("SERVER RUNNING .....")
-    const url = req.url
-    const method = req.method
-
-
-    if (req.url === "/") {
-        res.write('<html>')
-        const fileData = fs.readFileSync('message.txt')
-        res.write(`<body><h1>${fileData}</h1><form action ="/message" method="POST"><input name = "message" type="text"/> <button type ="submit">Send</button></form></body>`)
-        res.write('</html>')
-        return res.end()
-    }
-
-    if (url === "/message" && method === "POST") {
-
-        const body = []
-
-        req.on("data", (chunk) => {
-            body.push(chunk)
-        })
-        return req.on('end', () => {
-            const parseBody = Buffer.concat(body).toString()
-            const message = parseBody.split("=")[1]
-            fs.writeFile('message.txt', message, (err) => {
-                res.statusCode = 302
-                res.setHeader('location', '/')
-                return res.end()
-            })
-
-        })
-
-    }
-
-
-    res.write('<html>')
-    res.write('<body><h1> Hello this is a Message  page </h1></body>')
-    res.write('</html>')
-    res.end()
-
-
+    routes(req, res)
 })
 
-server.listen(4000)
+server.listen(3000)
